887cdbfdbd56815b2bf9fe3a072e3292ab2cea8f
Lee Yun Seok <leeyj6457@naver.com>
2025-04-03 22:07:33 +0900

    1일차 학습
    1. 기본 구조 개념
    2. XAML 내부 변환
    3. 구조와 데이터 관리 방식
    4. 데이터 바인딩

---
+++ Exercise1_ToDoList/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Educate_DotNetMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
